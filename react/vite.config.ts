e8c91b1414292278a9843ea3ef0f938d39518c53
jeff-stratofied <jeff@stratofied.io>
2026-03-09 17:41:38 -0400

    Update vite.config.ts

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/reporting-phase2/',
+  base: '/reporting-phase2/react/',
   build: {
     outDir: 'dist',
   },
